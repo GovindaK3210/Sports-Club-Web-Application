05ab14174a20a9b2d8ace0b38dc558f20d697fe3
Aqib Ahmad <aqib398@gmail.com>
2020-05-31 20:18:48 +0500

    updated in-site documentation and names of dummy coaches for ease

--- backend/generate-dummy-data.js
+++ backend/generate-dummy-data.js
@@ -12,7 +12,7 @@ console.log("===>>>> Inserting dummy data (coaches, admins) <<<<===")
 User.create( 
     {
         _id: mongoose.Types.ObjectId('154237499191442806967488'),
-        name: "Ahmed Raza (Coach)",
+        name: "Ahmed Raza (Coach1)",
         email: "coach1@coach.com",
         role: "coach",
         password: "123",
@@ -22,7 +22,7 @@ User.create(
     User.create( 
         {
             _id: mongoose.Types.ObjectId('822276808369006591404769'),
-            name: "Rao Ali (Coach)",
+            name: "Rao Ali (Coach2)",
             email: "coach2@coach.com",
             role: "coach",
             password: "123",
@@ -33,7 +33,7 @@ User.create(
     User.create( 
         {
             _id: mongoose.Types.ObjectId('914420173043840310350834'),
-            name: "Ahmed Asghar (Coach)",
+            name: "Ahmed Asghar (Coach3)",
             email: "coach3@coach.com",
             role: "coach",
             password: "123",
@@ -44,7 +44,7 @@ User.create(
     User.create( 
         {
             _id: mongoose.Types.ObjectId('871580485503263878323443'),
-            name: "Tehreem Farooq (Coach)",
+            name: "Tehreem Farooq (Coach4)",
             email: "coach4@coach.com",
             role: "coach",
             password: "123",
@@ -55,7 +55,7 @@ User.create(
     User.create( 
         {
             _id: mongoose.Types.ObjectId('362852240509076741746581'),
-            name: "Mobeen Ali (Coach)",
+            name: "Mobeen Ali (Coach5)",
             email: "coach5@coach.com",
             role: "coach",
             password: "123",
@@ -66,7 +66,7 @@ User.create(
     User.create( 
         {
             _id: mongoose.Types.ObjectId('463799269825050652338804'),
-            name: "Haider Bokhari (Coach)",
+            name: "Haider Bokhari (Coach6)",
             email: "coach6@coach.com",
             role: "coach",
             password: "123",
@@ -77,7 +77,7 @@ User.create(
     User.create( 
         {
             _id: mongoose.Types.ObjectId('882926639578545056232499'),
-            name: "Govinda Kumar (Coach)",
+            name: "Govinda Kumar (Coach7)",
             email: "coach7@coach.com",
             role: "coach",
             password: "123",
@@ -88,7 +88,7 @@ User.create(
     User.create( 
         {
             _id: mongoose.Types.ObjectId('121841186077525354630397'),
-            name: "Abid Ali (Coach)",
+            name: "Abid Ali (Coach8)",
             email: "coach8@coach.com",
             role: "coach",
             password: "123",
@@ -99,7 +99,7 @@ User.create(
     User.create( 
         {
             _id: mongoose.Types.ObjectId('800890683885239949450384'),
-            name: "Babar Azam (Coach)",
+            name: "Babar Azam (Coach9)",
             email: "coach9@coach.com",
             role: "coach",
             password: "123",
@@ -110,7 +110,7 @@ User.create(
     User.create( 
         {
             _id: mongoose.Types.ObjectId('145685655368288795245964'),
-            name: "Misbah Khan (Coach)",
+            name: "Misbah Khan (Coach10)",
             email: "coach10@coach.com",
             role: "coach",
             password: "123",
@@ -121,7 +121,7 @@ User.create(
     User.create( 
         {
             _id: mongoose.Types.ObjectId('776355791919436623385471'),
-            name: "Taimoor Aftab (Coach)",
+            name: "Taimoor Aftab (Coach11)",
             email: "coach11@coach.com",
             role: "coach",
             password: "123",
@@ -132,7 +132,7 @@ User.create(
     User.create( 
         {
             _id: mongoose.Types.ObjectId('702677462621390047737619'),
-            name: "Sara Khan (Coach)",
+            name: "Sara Khan (Coach12)",
             email: "coach12@coach.com",
             role: "coach",
             password: "123",
@@ -143,7 +143,7 @@ User.create(
     User.create( 
         {
             _id: mongoose.Types.ObjectId('994614194720061613483469'),
-            name: "Usman Atif (Coach)",
+            name: "Usman Atif (Coach13)",
             email: "coach13@coach.com",
             role: "coach",
             password: "123",
@@ -154,7 +154,7 @@ User.create(
     User.create( 
         {
             _id: mongoose.Types.ObjectId('170354616530645060165696'),
-            name: "Makhdoom Ali (Coach)",
+            name: "Makhdoom Ali (Coach14)",
             email: "coach14@coach.com",
             role: "coach",
             password: "123",
@@ -165,7 +165,7 @@ User.create(
     User.create( 
         {
             _id: mongoose.Types.ObjectId('581322468881331271035466'),
-            name: "Atir Qureshi (Coach)",
+            name: "Atir Qureshi (Coach15)",
             email: "coach15@coach.com",
             role: "coach",
             password: "123",
@@ -176,7 +176,7 @@ User.create(
     User.create( 
         {
             _id: mongoose.Types.ObjectId('746446515837952360789980'),
-            name: "Noman Rao (Coach)",
+            name: "Noman Rao (Coach16)",
             email: "coach16@coach.com",
             role: "coach",
             password: "123",
@@ -187,7 +187,7 @@ User.create(
     User.create( 
         {
             _id: mongoose.Types.ObjectId('023021429476642800682513'),
-            name: "Hajra Ali (Coach)",
+            name: "Hajra Ali (Coach17)",
             email: "coach17@coach.com",
             role: "coach",
             password: "123",
@@ -198,7 +198,7 @@ User.create(
     User.create( 
         {
             _id: mongoose.Types.ObjectId('102465529556702452541670'),
-            name: "Mesum Bokhari (Coach)",
+            name: "Mesum Bokhari (Coach18)",
             email: "coach18@coach.com",
             role: "coach",
             password: "123",
@@ -209,7 +209,7 @@ User.create(
     User.create( 
         {
             _id: mongoose.Types.ObjectId('837290345771834812213264'),
-            name: "Aqib Ahmad (Coach)",
+            name: "Aqib Ahmad (Coach19)",
             email: "coach19@coach.com",
             role: "coach",
             password: "123",
@@ -220,7 +220,7 @@ User.create(
     User.create( 
         {
             _id: mongoose.Types.ObjectId('970614340200118034653506'),
-            name: "Khalid Ali (Coach)",
+            name: "Khalid Ali (Coach20)",
             email: "coach20@coach.com",
             role: "coach",
             password: "123",
